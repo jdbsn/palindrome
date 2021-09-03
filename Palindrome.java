920e64e21c8d1cffabe6ef65eba8edeb5b33cf3b
João Neto <jbsn1@discente.ifpe.edu.br>
2021-09-03 10:36:05 -0300

    Major changes

--- Palindrome.java
+++ Palindrome.java
@@ -2,22 +2,30 @@ package Palindrome;
 
 public class Palindrome {
 
-	public static void main(String[] args) {
+	public boolean checkPalindrome (String original) {
 
-		String original = "nurses RuN";
-		String reverse = "";
+		original = original.replaceAll(" ", "").toLowerCase();
+		System.out.println(original);
 		
-			original = original.replaceAll(" ", "").toLowerCase();
-		
-		for (int a = original.length() - 1; a >= 0; a--) {
-			reverse += original.charAt(a);
-			reverse = reverse.replaceAll(" ", "").toLowerCase();
-		}
-		
-		if (original.equals(reverse)) {
-			System.out.println("Palindrome");
+		if (original.isBlank()) {
+			System.out.println("Blank");
+			return false;
 		} else {
-			System.out.println("Not a palindrome");
+			
+			String reverse = "";
+			
+			for (int a = original.length() - 1; a >= 0; a--) {
+				reverse += original.charAt(a);
+				reverse = reverse.replaceAll(" ", "").toLowerCase();
+			}
+			
+			if (original.equals(reverse)) {
+				System.out.println("Palindrome");
+				return true;
+			} else {
+				System.out.println("Not a palindrome");
+				return false;
+			}
 		}
 		
 	}
